a94d56199387b4761da2b0b93246eb782e0b652b
M. Fatih MAR <mfatihmar@gmail.com>
2020-08-24 11:12:19 +0100

    update install script

--- .install.sh
+++ .install.sh
@@ -1,11 +1,11 @@
 # Install Homebrew via curl
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
 
 brew install tree
 brew install ripgrep
 
 # Install oh-my-zsh via curl
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 
 # Install Antigen via curl
 curl -L git.io/antigen > antigen.zsh

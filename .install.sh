c024a8b784cec574343aa456f3732009a7a314a5
M. Fatih MAR <mfatihmar@gmail.com>
2020-08-24 12:32:28 +0100

    update install script

--- .install.sh
+++ .install.sh
@@ -4,6 +4,10 @@ curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
 brew install tree
 brew install ripgrep
 
+brew install go
+brew install llvm
+brew install clang-format
+
 # Install oh-my-zsh via curl
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 

d24346dbeaeb2db7cdde9d8cfade29841fbbdfcb
M. Fatih MAR <mfatihmar@gmail.com>
2020-09-07 10:53:09 +0100

    update install script

--- .install.sh
+++ .install.sh
@@ -1,5 +1,5 @@
 # Install Homebrew via curl
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install git
 brew install tree
@@ -10,7 +10,7 @@ brew install llvm
 brew install clang-format
 
 # Install oh-my-zsh via curl
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install Antigen via curl
 curl -L git.io/antigen > antigen.zsh
